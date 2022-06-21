57c33fdc5af856221ad6fed983f783014000d9d4
Aguiar575 <aguiarus99arthurus@gmail.com>
2022-06-20 21:57:35 -0300

    Lasagna quest

---
+++ CsharpQuests/CsharpQuests/Lucian's Luscious Lasagna.cs
@@ -0,0 +1,19 @@
+﻿namespace CsharpQuests;
+
+public class Lasagna
+{
+    private const int _expectedMinutesInOven = 40;
+    
+    public int GetExpectedMinutesInOven() => _expectedMinutesInOven;
+
+    public int RemainingMinutesInOven(int elapsed)
+    {
+        return _expectedMinutesInOven - elapsed; 
+    }
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
+    
+    public int ElapsedTimeInMinutes(int usedLayers, int minutesInOven)
+    {
+        return PreparationTimeInMinutes(usedLayers) + minutesInOven;
+    }
+}
